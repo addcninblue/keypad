84d4f7957ef633d89987c643605a0986f0be82a4
Addison Chan <addison95132@gmail.com>
2022-06-06 16:28:49 -0700

    Add wiring renders

--- src/keypad/core.clj
+++ src/keypad/core.clj
@@ -2,23 +2,50 @@
   (:require [scad-clj.scad :as scad])
   (:require [scad-clj.model :as model])
   (:require [keypad.layouts :as layouts])
-  (:require [keypad.lib :as lib]))
+  (:require [keypad.lib :as lib])
+  (:require [keypad.const :as const]))
+
+; TODO: Figure out stem height of rotary knob. If > 5mm, we're good
+(def top
+  (model/extrude-linear
+    {:height const/PLATE_HEIGHT
+     :center false}
+    (model/difference
+      (lib/add-border
+        (lib/generate-cluster layouts/keypad-layout :full))
+      (lib/generate-cluster layouts/keypad-layout :top))))
 
 (def plate
-  (model/difference
-    (lib/add-border
-      (lib/generate-cluster layouts/keypad-layout :full))
-    (lib/generate-cluster layouts/keypad-layout :plate)))
+  (model/extrude-linear
+    {:height const/PLATE_HEIGHT
+     :center false}
+    (model/difference
+      (lib/add-border
+        (lib/generate-cluster layouts/keypad-layout :full))
+      (lib/generate-cluster layouts/keypad-layout :plate))))
 
-(def top
-  (model/difference
+(def wiring
+  (model/extrude-linear
+    {:height const/PLATE_HEIGHT
+     :center false}
+    (model/difference
+      (lib/add-border
+        (lib/generate-cluster layouts/keypad-layout :full))
+      (lib/generate-cluster layouts/keypad-layout :wiring))))
+
+(def bottom
+  (model/extrude-linear
+    {:height const/BASE_HEIGHT
+     :center false}
     (lib/add-border
-      (lib/generate-cluster layouts/keypad-layout :full))
-    (lib/generate-cluster layouts/keypad-layout :top)))
+      (lib/generate-cluster layouts/keypad-layout :full))))
 
 (def parts
   {"top.scad" top
-   "plate.scad" plate})
+   "plate.scad" plate
+   "wiring.scad" wiring
+   "bottom.scad" bottom
+   })
 
 (defn -main
   [& _args]
