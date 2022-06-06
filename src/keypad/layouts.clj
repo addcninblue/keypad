c363e52b3323ee0cf95d86d3eb193124ec3d2715
Addison Chan <addison95132@gmail.com>
2022-06-06 16:04:43 -0700

    Add abstractions for space, knob

--- src/keypad/layouts.clj
+++ src/keypad/layouts.clj
@@ -3,3 +3,20 @@
 (def keypad-layout [[1, 1, 1],
                     [1, 1, 1],
                     [1, 1, 1]])
+
+(defn space
+  [width]
+  [:space, width])
+
+(defn space?
+  [space]
+  (and (vector? space) (= (first space) :space)))
+
+(defn space-width
+  [space]
+  (assert space? space)
+  (second space))
+
+(defn knob [] :knob)
+
+(defn knob? [elem] (= :knob elem))
