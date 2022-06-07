4c5af4d9dcb5883aa942364160b28d0489d9a22e
Addison Chan <addison95132@gmail.com>
2022-06-06 17:11:44 -0700

    Refactor layers

--- src/keypad/core.clj
+++ src/keypad/core.clj
@@ -15,23 +15,20 @@
         (lib/generate-cluster layouts/keypad-layout :full))
       (lib/generate-cluster layouts/keypad-layout :top))))
 
-(def plate
+(defn generate-layer
+  [cutout-type]
   (model/extrude-linear
     {:height const/PLATE_HEIGHT
      :center false}
     (model/difference
       (lib/add-border
         (lib/generate-cluster layouts/keypad-layout :full))
-      (lib/generate-cluster layouts/keypad-layout :plate))))
+      (lib/generate-cluster layouts/keypad-layout cutout-type))))
 
-(def wiring
-  (model/extrude-linear
-    {:height const/PLATE_HEIGHT
-     :center false}
-    (model/difference
-      (lib/add-border
-        (lib/generate-cluster layouts/keypad-layout :full))
-      (lib/generate-cluster layouts/keypad-layout :wiring))))
+; TODO: Figure out stem height of rotary knob. If > 5mm, we're good
+(def top (generate-layer :top))
+(def plate (generate-layer :plate))
+(def wiring (generate-layer :wiring))
 
 (def bottom
   (model/extrude-linear
@@ -40,11 +37,19 @@
     (lib/add-border
       (lib/generate-cluster layouts/keypad-layout :full))))
 
+(def full
+  (model/union
+    (model/translate [0 0 (+ const/BASE_HEIGHT const/PLATE_HEIGHT const/PLATE_HEIGHT)] top)
+    (model/translate [0 0 (+ const/BASE_HEIGHT const/PLATE_HEIGHT)] plate)
+    (model/translate [0 0 (+ const/BASE_HEIGHT)] wiring)
+    bottom))
+
 (def parts
   {"top.scad" top
    "plate.scad" plate
    "wiring.scad" wiring
    "bottom.scad" bottom
+   "full.scad" full
    })
 
 (defn -main
